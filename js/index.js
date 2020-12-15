a510f6a18df37027249a996dd6fef7bf230749ac
kodejalaid <70847887+kodejalaid@users.noreply.github.com>
2020-12-15 10:08:08 +0700

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,43 @@
+const selector = {
+  contLink : document.querySelector('.judul')
+};
+
+var typed = new Typed('.target', { // Typed JS
+  strings: ["Dimas Andhika", "@DIMAS.NGODING"],
+  typeSpeed: 50,
+  loop: true,
+  backDelay: 1000,
+  backSpeed: 30
+});
+
+const link = document.querySelector('.input form') // Tombol
+  .addEventListener('click', function() {
+    let valNomor = document.getElementById('nomor').value;
+    let valTeks = document.getElementById('teks').value;
+    let hasil = ``;
+    let unicode = valTeks.split(' ').join('%20');
+
+    if (valTeks !== '') {
+      hasil = `https://api.whatsapp.com/send?phone=${valNomor}&text=${unicode}`;
+    } else if (valTeks == '') {
+      hasil =  `https://api.whatsapp.com/send?phone=${valNomor}`;
+    }
+
+    selector.contLink.innerHTML = ``;
+    selector.contLink.innerHTML += /*html*/`
+    <h1><img src="img/img.png" alt="Whatsapp"> Direct-WA</h1>
+    <h3><span>Send Whatsapp</span> Message without saving the number</h3>
+    <a href="${hasil}" target="_blank">${hasil}</a>
+    `;
+    
+    setTimeout(() => {
+      window.open(hasil, '_blank');
+    }, 1500);
+
+    setTimeout(() => {
+      selector.contLink.innerHTML = /*html*/`
+      <h1><img src="img/img.png" alt="Whatsapp"> Direct-WA</h1>
+      <h3><span>Send Whatsapp</span> Message without saving the number</h3>
+      `
+    }, 3000);
+  });
