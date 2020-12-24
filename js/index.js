8e731746ea5c15d26a56e478e5be8a4ef68e4205
kodejalaid <70847887+kodejalaid@users.noreply.github.com>
2020-12-24 11:35:51 +0700

    Autentikasi Nomor

--- js/index.js
+++ js/index.js
@@ -17,27 +17,33 @@ const link = document.querySelector('.input form') // Tombol
     let hasil = ``;
     let spasi = valTeks.replace(/ /g, '%20').replace(/(?:\r|\n|\r\n)/g, '%0A');
 
-    if (valTeks !== '') {
-      hasil = `https://api.whatsapp.com/send?phone=${valNomor}&text=${spasi}`;
-    } else if (valTeks == '') {
-      hasil =  `https://api.whatsapp.com/send?phone=${valNomor}`;
-    }
+    if (!valNomor.includes('62')) {
+      alert('Nomormu salah!');
+      document.getElementById('nomor').value = '62';
+      document.getElementById('teks').value = '';
+    } else {
+      if (valTeks !== '') {
+        hasil = `https://api.whatsapp.com/send?phone=${valNomor}&text=${spasi}`;
+      } else if (valTeks == '') {
+        hasil =  `https://api.whatsapp.com/send?phone=${valNomor}`;
+      }
 
-    selector.contLink.innerHTML = ``;
-    selector.contLink.innerHTML += /*html*/`
-    <h1><img src="img/img.png" alt="Whatsapp"> Direct-WA</h1>
-    <h3><span>Send Whatsapp</span> Message without saving the number</h3>
-    <a href="${hasil}" target="_blank">${hasil}</a>
-    `;
-    
-    setTimeout(() => {
-      window.open(hasil, '_blank');
-    }, 1500);
-
-    setTimeout(() => {
-      selector.contLink.innerHTML = /*html*/`
+      selector.contLink.innerHTML = ``;
+      selector.contLink.innerHTML += /*html*/`
       <h1><img src="img/img.png" alt="Whatsapp"> Direct-WA</h1>
-      <h3><span>Send Whatsapp</span> Message without saving the number, by <span class="target"></span></h3>
-      `
-    }, 3000);
+      <h3><span>Send Whatsapp</span> Message without saving the number</h3>
+      <a href="${hasil}" target="_blank">${hasil}</a>
+      `;
+
+      setTimeout(() => {
+        window.open(hasil, '_blank');
+      }, 1500);
+
+      setTimeout(() => {
+        selector.contLink.innerHTML = /*html*/`
+        <h1><img src="img/img.png" alt="Whatsapp"> Direct-WA</h1>
+        <h3><span>Send Whatsapp</span> Message without saving the number, by <span class="target"></span></h3>
+        `
+      }, 3000);
+    }
   });
